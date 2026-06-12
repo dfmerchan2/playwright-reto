d1a99e9916786b1aadf0121ddc242a191fe029fc
Diego Merchan <dfmerchan2@gmail.com>
2026-06-12 12:15:02 -0500

    Test Navigation through the left panel

--- tests/navegation.spec.ts
+++ tests/navegation.spec.ts
@@ -30,3 +30,27 @@ test('Check left menu options', async ({page}) => {
     expect(currentMenuItems).toEqual(expectedMenuItems)
     expect(currentMenuItems[0]).toEqual(expectedMenuItems[0])
 })
+
+test('Navigation through the left panel', async ({page}) => {
+    await page.goto(URL_PAGE)
+    await page.getByRole('textbox', {name: 'username'}).fill('Admin')
+    await page.getByRole('textbox', {name: 'password'}).fill('admin123')
+    await page.getByRole('button', {name: 'Login'}).click()
+
+    await expect(page.getByRole('link', {name: 'Admin'})).toBeVisible()
+
+    const leftMenuItems = page.getByLabel('Sidepanel').getByRole('listitem')
+    const currentMenuItemCount = await leftMenuItems.count()
+
+    for (let i = 0; i < currentMenuItemCount; i++) {
+        const menuItem = leftMenuItems.nth(i)
+        const menuText = await menuItem.innerText()
+
+        console.log('Current menu item', menuText)
+        await menuItem.click()
+
+        if (menuText == 'Maintenance') {
+            await page.goBack();
+        }
+    }
+})
